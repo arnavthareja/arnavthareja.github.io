a51d6b21c1bdd82d8751dd69c0aad2752e7c2351
arnavthareja <arnav.thareja25@gmail.com>
2020-07-14 23:58:37 -0700

    Remove hover transform effect from project cards

--- assets/index.js
+++ assets/index.js
@@ -14,17 +14,6 @@ function select(id) {
     // update selected element
     selected = id;
 
-    // UPdate portfolio section title
+    // Update portfolio section title
     document.querySelector('#portfolio-title').innerHTML = selected;
 }
-
-// Translate each card up 5px on hover
-// This is the only way to use transform style on .project-card
-document.querySelectorAll('.project-card').forEach(function(card) {
-    card.addEventListener('mouseover', function() {
-        this.style.transform = 'translateY(-5px)';
-    });
-    card.addEventListener('mouseout', function() {
-        this.style.transform = 'translateY(0)';
-    });
-});
