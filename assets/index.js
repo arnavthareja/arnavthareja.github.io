4871be61a4955e4ab3e2308c546bc77a01e4cd8a
Arnav Thareja <arnav.thareja25@gmail.com>
2020-07-31 19:49:41 -0700

    Add dark mode

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
 var selected = 'Projects';
+var darkModeActive = false;
 
 function select(id) {
     
@@ -16,4 +17,39 @@ function select(id) {
 
     // Update portfolio section title
     document.querySelector('#portfolio-title').innerHTML = selected;
+}
+
+function toggleDarkMode() {
+
+    const root = document.documentElement;
+
+    if (darkModeActive) {
+        // Change to light mode
+        root.style.setProperty('--main-background-color', '#003262');
+        root.style.setProperty('--main-text-color', 'white');
+        root.style.setProperty('--main-background-gradient', 'linear-gradient(30deg, #003262, #004893)');
+        root.style.setProperty('--alt-background-color', '#d7cb84');
+        root.style.setProperty('--alt-text-color', '#003262');
+        root.style.setProperty('--box-shadow', '0 5px 15px black');
+        darkModeActive = false;
+
+        // Change dark mode button to light mode
+
+        
+        return console.log("Switched to light mode");
+    } else {
+        // Change to dark mode
+        root.style.setProperty('--main-background-color', 'black');
+        root.style.setProperty('--main-text-color', 'white');
+        root.style.setProperty('--main-background-gradient', 'black');
+        root.style.setProperty('--alt-background-color', 'black');
+        root.style.setProperty('--alt-text-color', 'white');
+        root.style.setProperty('--box-shadow', '0 0 5px white');
+        darkModeActive = true;
+
+        // Change dark mode button to dark mode
+
+        
+        return console.log("Switched to dark mode");
+    }
 }
